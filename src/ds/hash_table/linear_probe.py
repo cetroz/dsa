159d8ad68729739576c81feb3ca2e0584f573256
cetroz <raisin@keemail.me>
2025-04-05 15:54:59 +0500

    hash_table: add delete operator, fix typos + misc changes

--- src/ds/hash_table/linear_probe.py
+++ src/ds/hash_table/linear_probe.py
@@ -2,6 +2,7 @@ class HashTable:
     def __init__(self, size):
         self.size = size
         self.table = [None] * size
+        self.deleted = object()
 
     def get_hash(self, key):
         return hash(key) % self.size
@@ -12,15 +13,19 @@ class HashTable:
         self.table = [None] * self.size
 
         for item in old_table:
-            if item is not None:
+            if item is not None and item is not self.deleted:
                 self.insert(item[0], item[1])
 
     def insert(self, key, val):
-        if self.load() > 0.7:
+        if self.load_factor() > 0.7:
             self.resize()
 
         hash_index = self.get_hash(key)
-        if not self.table[hash_index] or self.table[hash_index][0] == key:
+        if (
+            self.table[hash_index] is None
+            or self.table[hash_index] is self.deleted
+            or self.table[hash_index][0] == key
+        ):
             self.table[hash_index] = (key, val)
             return
 
@@ -44,6 +49,28 @@ class HashTable:
                 break
 
         return None
+
+    def rehash(self, hash_index):
+        hash_index = (hash_index + 1) % self.size
+        while self.table[hash_index] is not None:
+            if self.table[hash_index] is not self.deleted:
+                key, value = self.table[hash_index]
+                self.table[hash_index] = self.deleted
+                self.insert(key, value)
+            hash_index = (hash_index + 1) % self.size
+
+    def delete(self, key):
+        hash_index = self.get_hash(key)
+        while self.table[hash_index] is not None:
+            if (
+                self.table[hash_index] is not self.deleted
+                and self.table[hash_index][0] == key
+            ):
+                self.table[hash_index] = self.deleted
+                self.rehash(hash_index)
+                return
+            hash_index = (hash_index + 1) % self.size
+        return
 
     def load_factor(self):
         return sum(1 for item in self.table if item is not None) / self.size
--- src/ds/hash_table/quad_probe.py
+++ src/ds/hash_table/quad_probe.py
@@ -2,9 +2,7 @@ class HashTable:
     def __init__(self, size):
         self.size = size
         self.table = [None] * size
-
-    def load_factor(self):
-        return sum(1 for item in self.table if item is not None) / self.size
+        self.deleted = object()
 
     def get_hash(self, key):
         return hash(key) % self.size
@@ -15,7 +13,7 @@ class HashTable:
         self.table = [None] * self.size
 
         for item in old_table:
-            if item is not None:
+            if item is not None and item is not self.deleted:
                 self.insert(item[0], item[1])
 
     def insert(self, key, val):
@@ -23,32 +21,61 @@ class HashTable:
             self.resize()
 
         hash_index = self.get_hash(key)
-        if self.table[hash_index] and self.table[hash_index][0] == key:
+        if (
+            self.table[hash_index] is None
+            or self.table[hash_index] is self.deleted
+            or self.table[hash_index][0] == key
+        ):
             self.table[hash_index] = (key, val)
             return
-        elif not self.table[hash_index]:
-            self.table[hash_index] = (key, val)
-            return
-        else:
-            original_index = hash_index
-            i = 1
-            while self.table[hash_index] is not None:
-                hash_index = (hash_index + i**2) % self.size
-                i += 1
-                if original_index == hash_index:
-                    self.resize()
-                    return self.insert(key, val)
-            self.table[hash_index] = (key, val)
+
+        original_index = hash_index
+        i = 1
+        while self.table[hash_index] is not None:
+            hash_index = (hash_index + i**2) % self.size
+            if original_index == hash_index:
+                self.resize()
+                return self.insert(key, val)
+        self.table[hash_index] = (key, val)
 
     def get(self, key):
         hash_index = self.get_hash(key)
         original_index = hash_index
         i = 1
+
         while self.table[hash_index] is not None:
             if self.table[hash_index][0] == key:
                 return self.table[hash_index][1]
             hash_index = (hash_index + i**2) % self.size
-            i += 1
             if original_index == hash_index:
                 break
+
         return None
+
+    def rehash(self, hash_index):
+        i = 1
+        hash_index = (hash_index + i**2) % self.size
+        while self.table[hash_index] is not None:
+            if self.table[hash_index] is not self.deleted:
+                key, value = self.table[hash_index]
+                self.table[hash_index] = self.deleted
+                self.insert(key, value)
+            hash_index = (hash_index + i**2) % self.size
+
+    def delete(self, key):
+        hash_index = self.get_hash(key)
+        i = 1
+        while self.table[hash_index] is not None:
+            if (
+                self.table[hash_index] is not self.deleted
+                and self.table[hash_index][0] == key
+            ):
+                self.table[hash_index] = self.deleted
+                self.rehash(hash_index)
+                return
+            hash_index = (hash_index + i**2) % self.size
+            i += 1
+        return
+
+    def load_factor(self):
+        return sum(1 for item in self.table if item is not None) / self.size
